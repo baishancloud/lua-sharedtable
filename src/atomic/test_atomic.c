edc4c844ae687a147ef0c003aeadde920adb0665
chenchuang <chuang.chen@baishancloud.com>
2017-12-03 21:35:05 +0800

    modify after code_review

--- src/atomic/test_atomic.c
+++ src/atomic/test_atomic.c
@@ -216,6 +216,44 @@ st_test(atomic, st_atomic_cas) {
     }
 
     free_buf(value, sizeof(int64_t));
+    free_buf(expect_v, sizeof(int64_t));
+}
+
+int block_all_children(int sem_id)
+{
+    union semun sem_args;
+    sem_args.val = 0;
+
+    int ret = semctl(sem_id, 0, SETVAL, sem_args);
+    if (ret == -1) {
+        return ST_ERR;
+    }
+
+    return ST_OK;
+}
+
+int wakeup_all_children(int sem_id, int children_num)
+{
+    struct sembuf sem = {.sem_num = 0, .sem_op = children_num, .sem_flg = SEM_UNDO};
+
+    int ret = semop(sem_id, &sem, 1);
+    if (ret == -1) {
+        return ST_ERR;
+    }
+
+    return ST_OK;
+}
+
+int wait_sem(int sem_id)
+{
+    struct sembuf sem = {.sem_num = 0, .sem_op = -1, .sem_flg = SEM_UNDO};
+
+    int ret = semop(sem_id, &sem, 1);
+    if (ret == -1) {
+        return ST_ERR;
+    }
+
+    return ST_OK;
 }
 
 int test_add_sub_in_process(int64_t init_v, int64_t step_v, int op)
@@ -229,11 +267,9 @@ int test_add_sub_in_process(int64_t init_v, int64_t step_v, int op)
         return ST_ERR;
     }
 
-    union semun sem_args;
-    sem_args.val = 0;
-    ret = semctl(sem_id, 0, SETVAL, sem_args);
-    if (ret == -1) {
-        return ST_ERR;
+    ret = block_all_children(sem_id);
+    if (ret == ST_ERR) {
+        return ret;
     }
 
     int64_t *value = alloc_buf(sizeof(int64_t));
@@ -246,13 +282,12 @@ int test_add_sub_in_process(int64_t init_v, int64_t step_v, int op)
         if (child == 0) {
             set_process_to_cpu(i);
 
-            struct sembuf sem = {.sem_num = 0, .sem_op = -1, .sem_flg = SEM_UNDO};
-            ret = semop(sem_id, &sem, 1);
-            if (ret == -1) {
-                return ST_ERR;
+            ret = wait_sem(sem_id);
+            if (ret == ST_ERR) {
+                return ret;
             }
 
-            for (int j = 0; j < 10000; j++) {
+            for (int j = 0; j < 100000000; j++) {
 
                 switch (op) {
                     case ST_ATOMIC_FETCH_ADD:
@@ -272,11 +307,11 @@ int test_add_sub_in_process(int64_t init_v, int64_t step_v, int op)
                         break;
 
                     case ST_ATOMIC_INC:
-                        st_atomic_inc(value);
+                        st_atomic_inc(value, step_v);
                         break;
 
                     case ST_ATOMIC_DEC:
-                        st_atomic_dec(value);
+                        st_atomic_dec(value, step_v);
                         break;
                 }
             }
@@ -289,10 +324,8 @@ int test_add_sub_in_process(int64_t init_v, int64_t step_v, int op)
 
     sleep(2);
 
-    struct sembuf sem = {.sem_num = 0, .sem_op = 10, .sem_flg = SEM_UNDO};
-    ret = semop(sem_id, &sem, 1);
-    if (ret == -1) {
-        ret = ST_ERR;
+    ret = wakeup_all_children(sem_id, 10);
+    if (ret != ST_OK) {
         goto error;
     }
 
@@ -304,9 +337,9 @@ int test_add_sub_in_process(int64_t init_v, int64_t step_v, int op)
     int64_t expect_v;
 
     if (op == ST_ATOMIC_FETCH_ADD || op == ST_ATOMIC_ADD || op == ST_ATOMIC_INC) {
-        expect_v = init_v + step_v * 10000 * 10;
+        expect_v = init_v + step_v * 100000000 * 10;
     } else {
-        expect_v = init_v - step_v * 10000 * 10;
+        expect_v = init_v - step_v * 100000000 * 10;
     }
 
     if (expect_v != *value) {
@@ -330,10 +363,12 @@ st_test(atomic, add_sub_in_multi_process) {
         {0, 3, ST_ATOMIC_FETCH_ADD},
         {0, 3, ST_ATOMIC_ADD},
         {0, 1, ST_ATOMIC_INC},
+        {0, 3, ST_ATOMIC_INC},
 
         {200000, 3, ST_ATOMIC_FETCH_SUB},
         {200000, 3, ST_ATOMIC_SUB},
         {200000, 1, ST_ATOMIC_DEC},
+        {200000, 3, ST_ATOMIC_DEC},
     };
 
     for (int i = 0; i < st_nelts(cases); i++) {
@@ -346,52 +381,52 @@ st_test(atomic, add_sub_in_multi_process) {
 
 st_test(atomic, store_load_in_multi_process) {
 
-    int i, child;
+    int i, child, ret;
     int pids[10] = {0};
 
     int64_t store_values[5] = {0xef, 0xefaf, 0x2ffefaff, 0xaf1ffbff3f9f, 0xafffffffffffffff};
     int64_t *value = alloc_buf(sizeof(int64_t));
     *value = store_values[0];
 
-    for (i = 0; i < 5; i++) {
+    int sem_id = semget(6789, 1, 06666|IPC_CREAT);
+    st_ut_ne(-1, sem_id, "");
+
+    st_ut_eq(ST_OK, block_all_children(sem_id), "");
+
+    for (i = 0; i < 10; i++) {
 
         child = fork();
 
         if (child == 0) {
+            int found = 0;
+            int64_t v;
+
             set_process_to_cpu(i);
 
-            for (int j = 0; j < 10000; j++) {
-                st_atomic_store(value, store_values[i%5]);
+            ret = wait_sem(sem_id);
+            if (ret == ST_ERR) {
+                exit(ret);
             }
 
-            exit(ST_OK);
-        }
+            for (int j = 0; j < 100000000; j++) {
 
-        pids[i] = child;
-    }
+                if (j % 2 == 0) {
+                    st_atomic_store(value, store_values[j % 5]);
 
-    for (i = 5; i < 10; i++) {
+                } else {
 
-        child = fork();
+                    v = st_atomic_load(value);
 
-        if (child == 0) {
-            set_process_to_cpu(i);
-
-            int64_t v;
-            int found = 0;
-
-            for (int j = 0; j < 10000; j++) {
-                v = st_atomic_load(value);
-
-                for (int k = 0; k < 5; k++) {
-                    if (v == store_values[k]) {
-                        found = 1;
-                        break;
+                    for (int k = 0; k < 5; k++) {
+                        if (v == store_values[k]) {
+                            found = 1;
+                            break;
+                        }
                     }
-                }
 
-                if (found == 0) {
-                    exit(ST_ERR);
+                    if (found == 0) {
+                        exit(ST_ERR);
+                    }
                 }
             }
 
@@ -403,19 +438,27 @@ st_test(atomic, store_load_in_multi_process) {
 
     sleep(2);
 
+    st_ut_eq(ST_OK, wakeup_all_children(sem_id, 10), "");
+
     st_ut_eq(ST_OK, wait_children(pids, 10), "");
 
+    semctl(sem_id, 0, IPC_RMID);
     free_buf(value, sizeof(int64_t));
 }
 
 st_test(atomic, swap_in_multi_process) {
 
-    int child, found;
+    int child, found, ret;
     int pids[10] = {0};
 
     int64_t store_values[5] = {0xef, 0xefaf, 0x2ffefaff, 0xaf1ffbff3f9f, 0xafffffffffffffff};
     int64_t *value = alloc_buf(sizeof(int64_t));
     *value = 0;
+
+    int sem_id = semget(7890, 1, 06666|IPC_CREAT);
+    st_ut_ne(-1, sem_id, "");
+
+    st_ut_eq(ST_OK, block_all_children(sem_id), "");
 
     for (int i = 0; i < 10; i++) {
 
@@ -424,10 +467,15 @@ st_test(atomic, swap_in_multi_process) {
         if (child == 0) {
             set_process_to_cpu(i);
 
-            for (int j = 0; j < 10000; j++) {
-                st_atomic_swap(value, store_values[i%5]);
+            ret = wait_sem(sem_id);
+            if (ret == ST_ERR) {
+                exit(ret);
+            }
 
-                st_atomic_cas(value, &store_values[i%5], store_values[(i+1)%5]);
+            for (int j = 0; j < 100000000; j++) {
+                st_atomic_swap(value, store_values[j % 5]);
+
+                st_atomic_cas(value, &store_values[j % 5], store_values[(j + 1) % 5]);
             }
 
             exit(ST_OK);
@@ -437,6 +485,8 @@ st_test(atomic, swap_in_multi_process) {
     }
 
     sleep(2);
+
+    st_ut_eq(ST_OK, wakeup_all_children(sem_id, 10), "");
 
     st_ut_eq(ST_OK, wait_children(pids, 10), "");
 
@@ -450,6 +500,7 @@ st_test(atomic, swap_in_multi_process) {
 
     st_ut_eq(1, found, "");
 
+    semctl(sem_id, 0, IPC_RMID);
     free_buf(value, sizeof(int64_t));
 }
 
